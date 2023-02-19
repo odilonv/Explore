59dbc0d39c557eedc47b69f8eb0be18148e95f75
Crhonopost <nath.souvignet@gmail.com>
2023-02-19 23:36:21 +0100

    gitignore test2 hihi

---
+++ src/Configuration/ConfigurationBDDPostgreSQL.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\PlusCourtChemin\Configuration;
+
+use Exception;
+use PDO;
+
+class ConfigurationBDDPostgreSQL implements ConfigurationBDDInterface
+{
+    private string $nomBDD = "iut";
+    private string $hostname = "162.38.222.142";
+
+    public function getLogin(): string
+    {
+        return "vidalo";
+    }
+
+    public function getMotDePasse(): string
+    {
+        return "test4";
+    }
+
+    public function getDSN() : string{
+        return "pgsql:host={$this->hostname};dbname={$this->nomBDD};options='--client_encoding=UTF8'";
+    }
+
+    public function getOptions() : array {
+        return array();
+    }
+}
