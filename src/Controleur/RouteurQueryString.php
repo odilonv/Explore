2c102b7b4f4a05003542b70bff6ae97df0ba4ff8
Esclapez Loïc <loicesclapez@gmail.com>
2023-03-27 00:00:11 +0200

    url

---
+++ src/Controleur/RouteurQueryString.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace TheFeed\Controleur;
+class RouteurQueryString
+{
+    public static function traiterRequete()
+    {
+
+
+        $action = $_REQUEST['action'] ?? 'feed';
+
+
+        $controleur = "publication";
+        if (isset($_REQUEST['controleur']))
+            $controleur = $_REQUEST['controleur'];
+
+        $controleurClassName = 'TheFeed\Controleur\Controleur' . ucfirst($controleur);
+
+        if (class_exists($controleurClassName)) {
+            if (in_array($action, get_class_methods($controleurClassName))) {
+                $controleurClassName::$action();
+            } else {
+                $controleurClassName::afficherErreur("Erreur d'action");
+            }
+        } else {
+            TheFeed\Controleur\ControleurGenerique::afficherErreur("Erreur de contrôleur");
+        }
+
+    }
+}
+
+?>
