43bac202a20e611b3ed3c3c2d86b557eff13d781
Odilon <odilon.vidal@icloud.com>
2023-04-10 00:46:49 +0200

    update

--- web/ressources/js/notifications.js
+++ web/ressources/js/notifications.js
@@ -1,16 +1,23 @@
 
 const notification = document.getElementById("notif");
+let exitCrossNotif = document.getElementById("icon-exit-notif")
+
+exitCrossNotif.addEventListener('click', () =>{
+    notification.classList.add('notification-hidden');
+    notification.classList.remove('notification-show');
+})
 
 // searchButton.addEventListener('click',notif);
 function notif(type, message){
     notification.classList.remove("notification-hidden");
-    document.querySelector('.messageFlash > p').textContent=message;
+    document.querySelector('.messageFlash div > p').textContent=message;
+    document.getElementById("imgnotif").src='../web/ressources/img/icons/'+ type +'-solid.svg';
     notification.classList.add("notification-show");
-    // On l'efface 8 secondes plus tard
+    // On l'efface 20 secondes plus tard
     setTimeout(() => {
         notification.classList.add('notification-hidden');
         notification.classList.remove('notification-show');
 
-    },8000);
+    },20000);
 }
 
