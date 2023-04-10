ce1f0a4f20568c6e558d160d2578dd5183fff2a1
Odilon <odilon.vidal@icloud.com>
2023-04-10 13:53:30 +0200

    message flash bueno adapté pour plusieurs villes

--- web/ressources/js/trajet.js
+++ web/ressources/js/trajet.js
@@ -3,31 +3,45 @@ const loader = document.getElementById('loader');
 
 buttonSearch.addEventListener('click', search);
 
+
 function search() {
     // Suppression de tous les objets de la carte
     map.removeObjects(map.getObjects());
 
     loader.style.display = 'block'; // Afficher le loader au début de la recherche
-
+    let cptvilles=0;
+    let distanceMsg=0;
+    let departMsg = document.getElementById('ville1').value;
     let villes = document.querySelectorAll(".inputVille");
+    let arriveeMsg = document.getElementById('ville'+villes.length).value;
+
+
     for(let i=1; i<villes.length; i++){
         let depart = villes[i-1].value;
         let arrivee = villes[i].value;
+
 
         let requete = new URL(`api/getPlusCourt/${depart}/${arrivee}`, document.baseURI);
         fetch(requete.href)
             .then(response => response.json())
             .then(data => {
                 addRoad(map, data.multiline);
-                notif('success',data.message);
-                loader.style.display = 'none'; // Masquer le loader une fois que addRoad est terminée
+                cptvilles++;
+
+                if(cptvilles===villes.length-1){
+                    distanceMsg+=data.distance;
+                    notif('success',"Le plus court chemin entre "+departMsg+" et "+arriveeMsg+" mesure "+ Math.round(distanceMsg) +" km.");
+                    loader.style.display = 'none'; // Masquer le loader une fois que addRoad est terminée
+                }
+                else {
+                    distanceMsg+=data.distance;
+                }
             })
             .catch(error => {
                 notif('danger',"Veuillez renseigner les champs avec des données valides.");
                 loader.style.display = 'none'; // Masquer le loader en cas d'erreur
             });
     }
-    console.log(villes.length);
 }
 
 function addRoad(map, points){
