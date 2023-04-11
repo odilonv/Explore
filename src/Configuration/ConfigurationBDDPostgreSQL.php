0e04f03825f7fee5362c462736c9490569f28388
Odilon <odilon.vidal@icloud.com>
2023-04-11 03:53:18 +0200

    Add files via upload

---
+++ src/Configuration/ConfigurationBDDPostgreSQL.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Explore\Configuration;
+
+use Exception;
+use PDO;
+
+class ConfigurationBDDPostgreSQL implements ConfigurationBDDInterface
+{
+    private string $nomBDD = "iut";
+    private string $hostname = "162.38.222.142";
+
+    public function getLogin(): string
+    {
+        return "";
+    }
+
+    public function getMotDePasse(): string
+    {
+        return "";
+    }
+
+    public function getDSN() : string{
+        return "pgsql:host={$this->hostname};dbname={$this->nomBDD};options='--client_encoding=UTF8'";
+    }
+
+    public function getOptions() : array {
+        return array();
+    }
+}
