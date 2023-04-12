01567ce5dd9441f17d884359586e185cfc90e093
Crhonopost <nath.souvignet@gmail.com>
2023-04-12 23:14:47 +0200

    gitignore ...

---
+++ src/Configuration/ConfigurationBDDPostgreSQL.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Explore\Configuration;
+
+use Exception;
+use PDO;
+
+class ConfigurationBDDPostgreSQL implements ConfigurationBDDInterface
+{
+    private string $nomBDD = "iut";
+    private string $hostname = "162.38.222.142";
+
+    public function getLogin(): string
+    {
+        return "souvignetn";
+    }
+
+    public function getMotDePasse(): string
+    {
+        return "060781121EK";
+    }
+
+    public function getDSN() : string{
+        return "pgsql:host={$this->hostname};dbname={$this->nomBDD};options='--client_encoding=UTF8'";
+    }
+
+    public function getOptions() : array {
+        return array();
+    }
+}
