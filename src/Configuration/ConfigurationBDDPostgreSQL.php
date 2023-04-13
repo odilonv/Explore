1b84054c6d555f965e9067ad9518232df8bb922e
Crhonopost <nath.souvignet@gmail.com>
2023-04-13 16:18:51 +0200

    correction port

--- src/Configuration/ConfigurationBDDPostgreSQL.php
+++ src/Configuration/ConfigurationBDDPostgreSQL.php
@@ -9,6 +9,7 @@ class ConfigurationBDDPostgreSQL implements ConfigurationBDDInterface
 {
     private string $nomBDD = "iut";
     private string $hostname = "162.38.222.142";
+    private string $port = "5673";
 
     public function getLogin(): string
     {
@@ -21,7 +22,7 @@ class ConfigurationBDDPostgreSQL implements ConfigurationBDDInterface
     }
 
     public function getDSN() : string{
-        return "pgsql:host={$this->hostname};dbname={$this->nomBDD};options='--client_encoding=UTF8'";
+        return "pgsql:host={$this->hostname};port={$this->port};dbname={$this->nomBDD};options='--client_encoding=UTF8'";
     }
 
     public function getOptions() : array {
